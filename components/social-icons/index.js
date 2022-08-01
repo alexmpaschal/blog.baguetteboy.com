e7d821ed21f03a7eef14db9c15438b68d465e4c2
yaboiwierdal <yaboiwierdal@gmail.com>
2022-07-31 21:50:34 -0400

    Fix footer icon sizing issues

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -6,12 +6,11 @@ const components = {
   github: Github,
 }
 
-const SocialIcon = ({ kind, href, size = 8 }) => {
+const SocialIcon = ({ kind, href, classNames="h-8 w-8" }) => {
   if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
     return null
 
   const SocialSvg = components[kind]
-  size = parseInt(size)
 
   return (
     <a
@@ -22,7 +21,7 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 ${claassNames}`}
       />
     </a>
   )
